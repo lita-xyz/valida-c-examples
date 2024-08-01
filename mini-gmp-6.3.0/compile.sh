f96132d286dfde6ed3c3be4a01bf861ccd67c56f
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2024-08-01 13:09:42 -0400

    wip: cross compile mini-gmp

--- mini-gmp-6.3.0/compile.sh
+++ mini-gmp-6.3.0/compile.sh
@@ -5,6 +5,8 @@ set -x
 
 # execute this script with env so that path of delendum toolchain is prepended to PATH
 
-clang -O0 -std=c11 -DMINI_GMP_DONT_USE_FLOAT_H -target delendum -I. -c mini-gmp.c -o mini-gmp.o
-clang -O0 -std=c11 -DMINI_GMP_DONT_USE_FLOAT_H -target delendum -I. -c mini-mpq.c -o mini-mpq.o
+CC=../../llvm-valida/build/bin/clang
+CFLAGS="-std=c11 -nostdinc -DMINI_GMP_DONT_USE_FLOAT_H -target delendum -I../../llvm-valida/build-libc/projects/libc/include -O0"
+$CC $CFLAGS -c mini-gmp.c -o mini-gmp.o
+$CC $CFLAGS -std=c11 -DMINI_GMP_DONT_USE_FLOAT_H -target delendum -I. -c mini-mpq.c -o mini-mpq.o
 
